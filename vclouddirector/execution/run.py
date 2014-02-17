61c5f4c26c5d1b764eaef9345ffce08609e2e5a2
peterhowells <peter.b.howells@gmail.com>
2014-02-17 13:30:43 -0600

    need to get this update in

--- vclouddirector/execution/run.py
+++ vclouddirector/execution/run.py
@@ -3,18 +3,18 @@ from vclouddirector.session.behaviors import SessionBehaviors
 from vclouddirector.model.org import Org
 
 
-sb = SessionBehaviors(url="https://vcd.lab.ord1.pc.rackspace.com/api")
-auth_token = sb.authenticate(user="qe_user", org="system", password="lYr5wGeN")
+sb = SessionBehaviors(url="https://sample/api")
+auth_token = sb.authenticate(user="test", org="test", password="test")
 
 default_headers = {'accept': 'application/*+xml;version=1.5', 'x-vcloud-authorization': auth_token}
 
-org = ActionClient(url="https://vcd.lab.ord1.pc.rackspace.com/api/org")
+org = ActionClient(url="https://sample/org")
 
 response = org.execute_get(headers=default_headers)
 
 #print response.content
 
-org1 = ActionClient(url="https://vcd.lab.ord1.pc.rackspace.com/api/org/51a0dd22-bc5c-403f-971f-fd6270a9972b")
+org1 = ActionClient(url="https://sample/api/org/51a0dd22-bc5c-403f-971f-fd6270a9972b")
 
 response = org1.execute_get(headers=default_headers)
 
@@ -23,11 +23,11 @@ response = org1.execute_get(headers=default_headers)
 #roleRetrieve = ActionClient(url="https://vcd.lab.ord1.pc.rackspace.com/api/admin")
 #print roleRetrieve.execute_get(headers=default_headers).content
 
-addUser = ActionClient(url="https://vcd.lab.ord1.pc.rackspace.com/api/admin/org/51a0dd22-bc5c-403f-971f-fd6270a9972b/users")
+addUser = ActionClient(url="https://sampleapi/admin/org/51a0dd22-bc5c-403f-971f-fd6270a9972b/users")
 
 xml_to_send = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><User xmlns=\"http://www.vmware.com/vcloud/v1.5\" name=\"createViaApiUser2\" >  <FullName>Example User Full Name</FullName> <EmailAddress>user@example.com</EmailAddress> <IsEnabled>true</IsEnabled> <Role href=\"https://vcd.lab.ord1.pc.rackspace.com/api/admin/role/ae910740-cbde-34ae-9d84-ef5c53880afe\" /> <Password>Pa55w0rd</Password> <GroupReferences /></User>"
 
-print addUser.execute_post(default_headers, xml_to_send).content
+#print addUser.execute_post(default_headers, xml_to_send).content
 
 
 
